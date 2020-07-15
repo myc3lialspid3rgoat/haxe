72ee1e3001d8cb3da6d3a17b579e8fd0724bb4f5
Simon Krajewski <simon@haxe.org>
2020-07-15 14:03:11 +0200

    generate unimplemented interface methods on abstract classes
    see #9716

--- tests/unit/src/unit/issues/Issue9619.hx
+++ tests/unit/src/unit/issues/Issue9619.hx
@@ -24,6 +24,27 @@ private class ConcreteOverloadChild extends AbstractOverloadParent {
 	override function abstractFunction(i:Int):Void {}
 }
 
+private interface InterfaceToBeImplemented {
+	@:overload function toBeImplemented():Bool;
+	@:overload public function toBeImplemented(i:Int):Int;
+}
+
+abstract class AbstractThatImplementsInterface implements InterfaceToBeImplemented {
+	public function new() {}
+}
+
+class ConcreteChildThatImplements extends AbstractThatImplementsInterface {
+	@:overload
+	public override function toBeImplemented() {
+		return true;
+	}
+
+	@:overload
+	public override function toBeImplemented(i:Int) {
+		return i * 2;
+	}
+}
+
 #end
 
 abstract private class AbstractParent {
@@ -45,6 +66,10 @@ class Issue9619 extends unit.Test {
 		#if (java || cs)
 		var cc = new ConcreteOverloadChild();
 		t(HelperMacros.typeError(new AbstractOverloadParent()));
+
+		var atii:AbstractThatImplementsInterface = new ConcreteChildThatImplements();
+		t(atii.toBeImplemented());
+		eq(24, atii.toBeImplemented(12));
 		#end
 		var cc = new ConcreteChild();
 
